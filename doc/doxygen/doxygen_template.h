e4ca4b52da460f62439a1de864adc27acddc839a
aggresss <aggresss@163.com>
2018-12-06 16:29:27 +0800

    2018-12-06 16:29:27 CST W49D4

---
+++ doc/doxygen/doxygen_template.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @author
+ * @copyright
+ * @brief
+ */
+
+
+
+
+/**
+ * @brief function brief
+ *
+ * @details
+ *
+ * @param xxx [in]
+ * @param yyy [in, out]
+ *
+ * @return 0 if return value >= 0
+ * @return Error code if return value < 0
+ *          - #XXXXX1 error 1
+ *               and more.
+ *          - #XXXXX2 error 2
+ *
+ * @see
+ *
+ * @attention
+ *
+ */
