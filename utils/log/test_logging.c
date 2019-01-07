fc2c74ade30e812125370dfe4755a9cf2ec7e6e3
aggresss <aggresss@163.com>
2019-01-07 11:18:38 +0800

    2019-01-07 11:18:37 CST W01D1

--- utils/log/test_logging.c
+++ utils/log/test_logging.c
@@ -4,7 +4,7 @@
 int main()
 {
     log_debug("test debug");
-    log_info("test info");
+    log_info("test info :%s", "args_test");
     log_warn("test warn");
     log_error("test error");
     char test_char[] = "test bin test bin";
