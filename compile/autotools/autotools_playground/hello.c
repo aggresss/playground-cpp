ad002415327e660ce2d7f7748975fba04c0cd930
aggresss <aggresss@163.com>
2019-01-26 16:00:37 +0800

    2019-01-26 16:00:35 CST W03D6

--- compile/autotools/autotools_playground/hello.c
+++ compile/autotools/autotools_playground/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "config.h"
+#include "version.h"
+
+
 int main(int argc, const char * argv[])
 {
-    printf("Hello, World!\n");
+    printf("TEST_VERSION_STRING: %s\n", TEST_VERSION_STRING);
 #ifdef ARG_TEST001
     printf("WITH ARG TEST001\n");
 #elif defined(ARG_TEST002)
