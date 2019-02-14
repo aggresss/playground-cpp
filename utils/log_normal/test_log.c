ba954fca347dcb362081859d5ee76e0809f15bf1
aggresss <aggresss@163.com>
2019-02-14 13:29:08 +0800

    2019-02-14 13:29:07 CST W06D4

--- utils/log_normal/test_log.c
+++ utils/log_normal/test_log.c
@@ -6,7 +6,7 @@ int main()
     //SetLogLevel(LOG_LEVEL_TRACE);
     LogTrace("test trace");
     LogDebug("test debug");
-    LogInfo("test info :%s", "args_test");
+    LogInfo("test info: %s", "args_test");
     LogWarn("test warn");
     LogError("test error");
     return 0;
