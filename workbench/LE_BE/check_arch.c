5abac5062a1f7356ee8e684b66a71ca52935b7e9
aggresss <aggresss@163.com>
2019-09-07 08:37:50 +0800

    2019-09-07 08:37:50 CST W35D6

--- workbench/LE_BE/check_arch.c
+++ workbench/LE_BE/check_arch.c
@@ -3,6 +3,18 @@
    architecture is little endian or big endian.
  */
 
+/*
+   Function check_for_endianness() returns 1, if architecture
+   is little endian, 0 in case of big endian.
+ */
+
+int check_for_endianness()
+{
+  unsigned int x = 1;
+  char *c = (char*) &x;
+  return (int)*c;
+}
+
 #include <stdio.h>
 int main ()
 {
