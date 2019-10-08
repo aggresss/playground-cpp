280095c63e4d3ef2ef085f4a0e518ac38691d7e0
aggresss <aggresss@163.com>
2019-10-08 14:58:39 +0800

    2019-10-08 14:58:39 CST W40D2

--- workbench/segfault/segfault_hello.c
+++ workbench/segfault/segfault_hello.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <execinfo.h>
+
 
 #define DUMP(func, call) printf("%s: func = %p, called by = %p\n", __FUNCTION__, func, call)
 
@@ -14,9 +19,40 @@ __cyg_profile_func_exit(void *this_func, void *call_site)
 }
 
 
+static void SignalSegFaultHandler(int sig)
+{
+    void *array[16];
+    size_t bt_size;
+    char **bt_strings;
+    size_t i;
+
+    printf("Segmentfault signal captured.\n");
+
+    bt_size = backtrace(array, 16);
+    bt_strings = backtrace_symbols(array, bt_size);
+    if (NULL == bt_strings) {
+        perror("backtrace_synbols");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Obtained %zd stack frames.\n", bt_size);
+
+    for (i = 0; i < bt_size; i++) {
+        printf("\t%s\n", bt_strings[i]);
+    }
+
+    free(bt_strings);
+    bt_strings = NULL;
+
+    exit(EXIT_FAILURE);
+}
+
+
 int main()
 {
+    signal(SIGSEGV, SignalSegFaultHandler);
     puts("Hello World!");
+    char *test = strdup(NULL);
     return 0;
 }
 
