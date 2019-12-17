66abf09f28ee591107275f345d3f98bb144ae9c9
aggresss <aggresss@163.com>
2019-12-17 14:47:10 +0800

    2019-12-17 14:47:10 CST W50D2

--- workbench/segfault/dis_func.sh
+++ workbench/segfault/dis_func.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #disassemble a function
 
+#CROSS=arm-hisiv300-linux-
+NM=${CROSS}nm
+OBJDUMP=${CROSS}objdump
+
 routine=$1
 func=$2
 
@@ -8,13 +12,13 @@ if [ -z "$routine" ]; then
     exit
 fi
 
-start=$(nm -n $routine | grep "\w\s$func\$" | awk '{print "0x"$1;exit}')
-end=$(nm -n $routine | grep -A1 "\w\s$func\$" | awk '{getline; print "0x"$1;exit}')
+start=$(${NM} -n $routine | grep "\w\s$func\$" | awk '{print "0x"$1;exit}')
+end=$(${NM} -n $routine | grep -A1 "\w\s$func\$" | awk '{getline; print "0x"$1;exit}')
 
 if [ -z "$func" ]; then
-    objdump -d $routine
+    ${OBJDUMP} -d $routine
 else
     echo "start-address: $start, end-address: $end"
-    objdump -d $routine --start-address=$start --stop-address=$end
+    ${OBJDUMP} -d $routine --start-address=$start --stop-address=$end
 fi
 
