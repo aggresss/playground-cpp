31366900f608e651260358795f79f440d81df9a1
aggresss <aggresss@163.com>
2020-09-10 16:21:36 +0800

    2020-09-10 16:21:36 CST W36D4

--- library/json_test/src/to_bson.cpp
+++ library/json_test/src/to_bson.cpp
@@ -9,7 +9,7 @@ int main()
     // create a JSON value
     json j = R"({"compact": true, "schema": 0})"_json;
 
-    j["bin"] = json::binary({1, 2, 3});
+    j["bin"] = json::binary({65, 66, 67});
 
     // serialize it to BSON
     std::vector<uint8_t> v = json::to_bson(j);
@@ -20,4 +20,15 @@ int main()
         std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << (int)byte << " ";
     }
     std::cout << std::endl;
+
+    json x = json::from_bson(v);
+    auto xIt = x.find("bin");
+    if (xIt != x.end() && xIt->is_binary()) {
+        auto& dump = xIt->get_binary();
+        std::cout << "length: " << dump.size() << std::endl;
+        for (auto& byte: dump) {
+            std::cout << byte << std::endl;
+        }
+    }
+
 }
---
+++ workbench/functor/README.md
@@ -0,0 +1,3 @@
+
+### Reference
+- https://www.cnblogs.com/decade-dnbc66/p/5347088.html
