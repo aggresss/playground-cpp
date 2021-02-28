197c8cb8dd47858504b1a72bdc258c5ce8b76c83
aggresss <aggresss@163.com>
2021-02-28 19:45:22 +0800

    2021-02-28 19:45:22 CST W09D0

---
+++ library/lua_test/src/hello.lua
@@ -0,0 +1,3 @@
+function PrintHelloLua()
+    print("hello !!!")
+end
--- library/lua_test/src/lua_test.c
+++ library/lua_test/src/lua_test.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // 创建lua运行环境
+    lua_State *luaEnv = lua_open();
+    luaopen_base(luaEnv);
+    luaL_openlibs(luaEnv);
+    if (!luaEnv) {
+        return -1;
+    }
+
+    // 载入lua文件
+    int loadInfo = luaL_loadfile(luaEnv, "hello.lua");
+    if (loadInfo) {
+        return loadInfo;
+    }
+    // 执行lua文件
+    lua_pcall(luaEnv, 0, 0, 0);
+
+    // 调用PrintHelloLua函数
+    lua_getglobal(luaEnv, "PrintHelloLua");
+    lua_pcall(luaEnv, 0, 0, 0);
     return 0;
 }
