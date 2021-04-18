2729ee1b5a5d747bf9ee7313f4ab8132f52a3776
aggresss <aggresss@163.com>
2021-04-18 08:42:39 +0800

    2021-04-18 08:42:39 CST W16D0

--- workbench/move_forward/forward.cpp
+++ workbench/move_forward/forward.cpp
@@ -1,27 +1,24 @@
-#include <utility>
 #include <iostream>
+#include <utility>
 
-void bar(const int& x)
-{
-  std::cout << "lvalue" << std::endl;
+void bar(const int& x) {
+    std::cout << "lvalue" << std::endl;
 }
 
-void bar(int&& x)
-{
-  std::cout << "rvalue" << std::endl;
+void bar(int&& x) {
+    std::cout << "rvalue" << std::endl;
 }
 
 template <typename T>
-void foo(T&& x)
-{
-  bar(x); // x 在 bar 函数内部由右值变成了左值
-  bar(std::forward<T>(x));
+void foo(T&& x) {
+    bar(x);  // x 在 bar 函数内部由右值变成了左值
+    bar(std::forward<T>(x));
 }
 
-int main()
-{
-  int x = 10;
-  foo(x);
-  foo(10);
-  return 0;
+int main() {
+    int x = 10;
+    foo(x);
+    foo(10);
+
+    return 0;
 }
--- workbench/move_forward/move_construction.cpp
+++ workbench/move_forward/move_construction.cpp
@@ -1,23 +1,24 @@
-#include <string>
-#include <iostream>
-#include <memory>
-#include <vector>
 #include <stdio.h>
 #include <string.h>
 
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
 class CMyString {
-private:
-    char * buf;
+   private:
+    char* buf;
     int len;
 
-private:
+   private:
     void copy(const char* s) {
-        buf = new char[len+1];
+        buf = new char[len + 1];
         memcpy(buf, s, len);
         buf[len] = '\0';
     }
 
-public:
+   public:
     CMyString() {
         std::cout << "构造函数" << std::endl;
 
@@ -31,8 +32,7 @@ public:
 
             buf = nullptr;
             len = 0;
-        }
-        else {
+        } else {
             std::cout << "构造函数" << str << std::endl;
             len = strlen(str);
             copy(str);
@@ -54,8 +54,6 @@ public:
         len = str.len;
         buf = str.buf;
 
-
-
         str.len = 0;
         str.buf = nullptr;
     }
@@ -76,13 +74,10 @@ public:
         return *this;
     }
 
-
-
     CMyString& operator=(CMyString&& str) {
         std::cout << "移动赋值函数" << str.buf << std::endl;
 
         if (this != &str) {
-
             if (buf != nullptr) {
                 delete[] buf;
                 buf = nullptr;
@@ -98,12 +93,9 @@ public:
     }
 
     ~CMyString() {
-        if (buf == nullptr)
-        {
+        if (buf == nullptr) {
             std::cout << "析构函数" << std::endl;
-        }
-        else
-        {
+        } else {
             std::cout << "析构函数" << buf << std::endl;
             delete[] buf;
             buf = nullptr;
@@ -116,12 +108,9 @@ public:
         else
             std::cout << "buf is null" << std::endl;
     }
-
-
 };
 
 void func1(CMyString str) {
-
 }
 
 CMyString func2() {
@@ -132,21 +121,20 @@ CMyString func2() {
 void test0() {
     CMyString s1("12");
 
-    func1(s1); //对象str尚未初始化，会调用拷贝构造函数
+    func1(s1);  //对象str尚未初始化，会调用拷贝构造函数
 
-    CMyString s2 = func2(); // 对象s2尚未初始化，会产生临时对象，调用移动构造函数
-
+    CMyString s2 = func2();  // 对象s2尚未初始化，会产生临时对象，调用移动构造函数
 
     CMyString s3 = "56";
 
-    s3 = s1; //对象s3已初始化，会调用拷贝赋值函数
+    s3 = s1;  //对象s3已初始化，会调用拷贝赋值函数
 }
 
 void test1() {
     CMyString s4 = "78";
     std::vector<CMyString> v1;
     //v1.push_back(s4);
-    v1.push_back(std::move(s4)); // 对象尚未初始化，会调用移动构造函数
+    v1.push_back(std::move(s4));  // 对象尚未初始化，会调用移动构造函数
 
     std::cout << "开始输出v1\n";
     for (auto& str : v1)
@@ -165,13 +153,11 @@ void test1() {
 }
 
 void test2() {
-
     CMyString s5 = "9";
-    s5 = func2(); // 对象s5已初始化， 会产生临时对象，调用移动赋值函数
+    s5 = func2();  // 对象s5已初始化， 会产生临时对象，调用移动赋值函数
 }
 
-int main(void)
-{
+int main(void) {
     std::cout << "begin test0()" << std::endl;
     test0();
     std::cout << std::endl;
@@ -185,4 +171,3 @@ int main(void)
 
     return 0;
 }
-
