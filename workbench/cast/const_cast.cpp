e379776224b111a7945f2bd4b661497bb465e7f5
aggresss <aggresss@163.com>
2021-04-20 21:18:57 +0800

    2021-04-20 21:18:57 CST W16D2

--- workbench/cast/const_cast.cpp
+++ workbench/cast/const_cast.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(void) {
 	int variable = 21;
 	const int* const_p = &variable;
-    int* modifier = const_p;
-	//int* modifier = const_cast<int *>(const_p);
+    // int* modifier = const_p;
+	int* modifier = const_cast<int *>(const_p);
 
 	*modifier = 7;
 	cout << "variable:" << variable << endl;
